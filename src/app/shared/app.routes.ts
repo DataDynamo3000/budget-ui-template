180f82b484eb467aeedf5da502bcbb77803b1bf7
DESKTOP-OQE7FO8\Marcus <marcus.wild@lernen.siw.swiss>
2024-10-31 20:52:46 +0100

    appRoutes

--- src/app/shared/app.routes.ts
+++ src/app/shared/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { categoriesPath } from '../category/category.routes';
 import { expensesPath } from '../expense/expense.routes';
+import { authGuard } from './guard/auth.guard';
 
 export const loginPath = 'login';
 export const defaultPath = categoriesPath; // TODO: switch to ex
+export const loginPath = 'login';
 
 const appRoutes: Routes = [
   {
@@ -23,5 +25,19 @@ const appRoutes: Routes = [
     path: '**',
     redirectTo: defaultPath
   }
+  {
+    path: loginPath,
+    loadComponent: () => import('./component/login/login.component')
+  }
+  {
+    path: categoriesPath,
+    loadChildren: () => import('../category/category.routes'),
+    canActivate: [authGuard]
+  },
+  {
+    path: expensesPath,
+    loadChildren: () => import('../expense/expense.routes'),
+    canActivate: [authGuard]
+  },
 ];
 export default appRoutes;
