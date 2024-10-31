77207816d622225c4519dbe0b21905409a866a80
DESKTOP-OQE7FO8\Marcus <marcus.wild@lernen.siw.swiss>
2024-10-31 20:56:16 +0100

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ import appRoutes from './app/shared/app.routes';
 import { registerLocaleData } from '@angular/common';
 import { PageTitleStrategy } from './app/shared/service/page-title-strategy.service';
 import AppComponent from './app/app.component';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './app/shared/interceptor/auth.interceptor';
 
 if (environment.production) enableProdMode();
 
@@ -16,11 +19,15 @@ registerLocaleData(locale);
 
 bootstrapApplication(AppComponent, {
   providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environment['firebaseConfig'] }
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'CHF' },
     { provide: LOCALE_ID, useValue: 'de-CH' },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: TitleStrategy, useClass: PageTitleStrategy },
     provideIonicAngular(),
     provideRouter(appRoutes, withPreloading(PreloadAllModules))
+  ]
+  providers: [
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 }).catch(err => console.error(err));
