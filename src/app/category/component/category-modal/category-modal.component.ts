32a88579f577fa866a5041b855bc1105c725f5c5
DESKTOP-OQE7FO8\Marcus <marcus.wild@lernen.siw.swiss>
2024-11-12 20:51:15 +0100

    Kategorie erstellen

--- src/app/category/component/category-modal/category-modal.component.ts
+++ src/app/category/component/category-modal/category-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import {
   IonButton,
   IonButtons,
@@ -11,11 +11,17 @@ import {
   IonItem,
   IonTitle,
   IonToolbar,
-  ModalController
+  ModalController,
+  ViewDidEnter
 } from '@ionic/angular/standalone';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { close, save, text, trash } from 'ionicons/icons';
+import { CategoryService } from '../../service/category.service';
+import { ToastService } from '../../../shared/toast.service';
+import { LoadingIndicatorService } from '../../../shared/service/loading-indicator.service';
+import { CategoryUpsertDto } from '../../../shared/domain';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-category-modal',
@@ -38,13 +44,26 @@ import { close, save, text, trash } from 'ionicons/icons';
     IonFabButton
   ]
 })
-export default class CategoryModalComponent {
+export default class CategoryModalComponent implements ViewDidEnter {
   // DI
+  private readonly categoryService = inject(CategoryService);
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly loadingIndicatorService = inject(LoadingIndicatorService);
   private readonly modalCtrl = inject(ModalController);
+  private readonly toastService = inject(ToastService);
+  readonly categoryForm = this.formBuilder.group({
+    id: [null! as string], // hidden
+    name: ['', [Validators.required, Validators.maxLength(40)]]
+  });
+  @ViewChild('nameInput') nameInput?: IonInput;
 
   constructor() {
     // Add all used Ionic icons
     addIcons({ close, save, text, trash });
+  }
+
+  ionViewDidEnter(): void {
+    this.nameInput?.setFocus();
   }
 
   cancel(): void {
@@ -52,7 +71,19 @@ export default class CategoryModalComponent {
   }
 
   save(): void {
-    this.modalCtrl.dismiss(null, 'save');
+    this.loadingIndicatorService.showLoadingIndicator({ message: 'Saving category' }).subscribe(loadingIndicator => {
+      const category = this.categoryForm.value as CategoryUpsertDto;
+      this.categoryService
+        .upsertCategory(category)
+        .pipe(finalize(() => loadingIndicator.dismiss()))
+        .subscribe({
+          next: () => {
+            this.toastService.displaySuccessToast('Category saved');
+            this.modalCtrl.dismiss(null, 'refresh');
+          },
+          error: error => this.toastService.displayWarningToast('Could not save category', error)
+        });
+    });
   }
 
   delete(): void {
